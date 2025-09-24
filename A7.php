77ecc527d5ce2f964c61b072ff657b5ab077fcb4
Oier <ogarciam23@fpsanturtzilh.eus>
2025-09-24 14:19:50 +0200

    A7.php hasita

---
+++ A7.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="eu">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Ariketa 7</title>
+</head>
+
+<style>
+    table,
+    th,
+    td {
+        border: 1px solid;
+    }
+</style>
+
+<body>
+    <form> <!-- <form action="welcome.php" method="post"> -->
+        Izena: <input type="text" name="izena"><br>
+        Abizena: <input type="text" name="abizena"><br>
+        Alokatutako liburua: <input type="text" name="alokatutako_liburua"><br>
+        Emaila: <input type="text" name="emaila"><br>
+        Alkera data: <input type="date" name="alokera_data"><br>
+        NAN zenbakia: <input type="text" name="nan"><br>
+        <input type="submit">
+    </form>
+
+
+</body>
+
+<?php
+
+
+
+?>
